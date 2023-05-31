f5635cf28e4d80a4cd4692c824d998f2f5286a09
Mai Thi <20130120@st.hcmuaf.edu.vn>
2023-05-31 16:07:01 +0700

    form đăng kí

--- src/component/buttons/Login.js
+++ src/component/buttons/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     MDBBtn,
     MDBContainer,
@@ -11,6 +11,7 @@ import {
     MDBCheckbox
 }
     from 'mdb-react-ui-kit';
+
 function Login() {
     return (
         <>
@@ -19,18 +20,19 @@ function Login() {
                 <MDBRow>
 
                     <MDBCol col='10' md='6'>
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image" />
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                             class="img-fluid" alt="Phone image"/>
                     </MDBCol>
 
                     <MDBCol col='4' md='6'>
 
                         <h2 className="fw-bold mb-2 text-center">Đăng nhập</h2>
-                        <MDBInput wrapperClass='mb-4' label='Nhập Email' id='formControlLg' type='email' size="lg" />
-                        <MDBInput wrapperClass='mb-4' label='Mật khẩu' id='formControlLg' type='password' size="lg" />
+                        <MDBInput wrapperClass='mb-4' label='Nhập Email' id='formControlLg' type='email' size="lg"/>
+                        <MDBInput wrapperClass='mb-4' label='Mật khẩu' id='formControlLg' type='password' size="lg"/>
 
 
                         <div className="d-flex justify-content-between mx-4 mb-4">
-                            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Nhớ mật khẩu' />
+                            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Nhớ mật khẩu'/>
                             <a href="!#">Quên mật khẩu?</a>
                         </div>
 
@@ -40,13 +42,13 @@ function Login() {
                             <p className="text-center fw-bold mx-3 mb-0">hoặc</p>
                         </div>
 
-                        <MDBBtn className="mb-2 w-100" size="lg" style={{ backgroundColor: '#dd4b39' }}>
-                            <MDBIcon  className="mx-2" />
+                        <MDBBtn className="mb-2 w-100" size="lg" style={{backgroundColor: '#dd4b39'}}>
+                            <MDBIcon className="mx-2"/>
                             Đăng nhập với google
                         </MDBBtn>
 
-                        <MDBBtn className="mb-4 w-100" size="lg" style={{ backgroundColor: '#3b5998' }}>
-                            <MDBIcon className="mx-2" />
+                        <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#3b5998'}}>
+                            <MDBIcon className="mx-2"/>
                             Đăng nhập với facebook
                         </MDBBtn>
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
