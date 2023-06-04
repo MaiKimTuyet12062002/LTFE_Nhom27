5911cea458c2a8e64df819feb13b8a340f047fb5
MaiKimTuyet12062002 <kimtuyet12062002@gmail.com>
2023-06-05 01:47:18 +0700

    silder

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,19 +3,43 @@ import Products from "./Products";
 import "./css/home.css"
 
 const Home = () => {
-  return (
-    <div className="hero">
-      <div className="card bg-dark text-white border-0">
-        <img
-          src="/assets/slider/banner-01.jpg"
-          className="card-img"
-          alt="Background"
-          height="550px"
-        />
-      </div>
-      <Products/>
-    </div>
-  );
+    return (
+        <div className="hero">
+            <div className="card bg-dark text-white border-0">
+                <img
+                    src="/assets/slider/banner-01.jpg"
+                    className="card-img-1"
+                    alt="Background"
+                    height="550px"
+                />
+                <img
+                    src="/assets/slider/banner-02.jpg"
+                    className="card-img-2"
+                    alt="Background"
+                    height="550px"
+                />
+                <img
+                    src="/assets/slider/banner-03.jpg"
+                    className="card-img-3"
+                    alt="Background"
+                    height="550px"
+                />
+                <img
+                    src="/assets/slider/banner-04.jpg"
+                    className="card-img-4"
+                    alt="Background"
+                    height="550px"
+                />
+                <img
+                    src="/assets/slider/banner-05.jpg"
+                    className="card-img-5"
+                    alt="Background"
+                    height="550px"
+                />
+            </div>
+            <Products/>
+        </div>
+    );
 };
 
 export default Home;
