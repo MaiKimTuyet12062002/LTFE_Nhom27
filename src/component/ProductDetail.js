576f6e4f44b9ca6475bd045c7dbe6933dcf1b98c
MaiKimTuyet12062002 <kimtuyet12062002@gmail.com>
2023-06-10 00:51:14 +0700

    sua form dk

--- src/component/ProductDetail.js
+++ src/component/ProductDetail.js
@@ -39,7 +39,7 @@ const ProductDetail = () => {
                         <h1 className="display-5 fw-bold">{product.name}</h1>
                         <hr />
                         <h2 className="my-4 text-center" style={{color: 'red', fontSize: '30px' }}>{product.price}đ</h2>
-                        <p className="lead">{product.des}</p>
+                        <p className="lead" style={{color: 'black'}}>{product.des}</p>
                         <button onClick={()=>handleCart(product)} className="btn btn-outline-primary my-5">{cartBtn}</button>
                     </div>
                 </div>
