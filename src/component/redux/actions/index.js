86041f0ed60958bcdbe04a2a09dea6760e292e10
Mai Thi <20130120@st.hcmuaf.edu.vn>
2023-06-09 18:51:21 +0700

    LocalStore

--- src/component/redux/actions/index.js
+++ src/component/redux/actions/index.js
@@ -1,20 +1,28 @@
 export const addItem = (product) => {
     return {
-        type : "ADDITEM",
-        payload : product
+        type: "ADDITEM",
+        payload: product
     }
 }
 
 export const delItem = (product) => {
     return {
-        type : "DELITEM",
-        payload : product
+        type: "DELITEM",
+        payload: product
     }
 }
 
-export const decItem=(product)=>{
-    return{
+export const decItem = (product) => {
+    return {
         type: "DEC_ITEM",
-        payload:product
+        payload: product
     }
 }
+// actions.js
+
+export const setCartItems = (cartItems) => {
+    return {
+        type: 'SET_CART_ITEMS',
+        payload: cartItems,
+    };
+};
