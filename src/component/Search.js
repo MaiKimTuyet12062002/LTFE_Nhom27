07e60fc2c35176b4b8207add1a2b4041cfb8cf80
MaiKimTuyet12062002 <kimtuyet12062002@gmail.com>
2023-06-10 14:02:52 +0700

    phan trang

--- src/component/Search.js
+++ src/component/Search.js
@@ -22,7 +22,6 @@ export default function SearchBar({onSearch}) {
                           d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
                 </svg>
             </button>
-
         </div>
     );
 }
