2e38cfdf06e1db79cb67304852759f9c5778912e
Mai Thi <20130120@st.hcmuaf.edu.vn>
2023-06-21 00:11:45 +0700

    chỉnh lại giao diện

--- src/component/Home.js
+++ src/component/Home.js
@@ -47,37 +47,17 @@ const Home = () => {
                         <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/8-col/img%285%29.jpg"
                              class="d-block w-100" alt="Exotic Fruits"/>
                         <div class="mask" style={{background: "rgba(0, 0, 0, 0.4)"}}></div>
-                        <div class="carousel-caption d-none d-md-block mb-5">
-                            <h1 class="mb-4">
-                                <strong>Learn Bootstrap 5 with MDB</strong>
-                            </h1>
-
-                            <p>
-                                <strong>Best & free guide of responsive web design</strong>
-                            </p>
-
-                            <p class="mb-4 d-none d-md-block">
-                                <strong>The most comprehensive tutorial for the Bootstrap 5. Loved by over 3 000 000
-                                    users. Video and written versions
-                                    available. Create your own, stunning website.</strong>
-                            </p>
-
-                            <a target="_blank" href="https://mdbootstrap.com/education/bootstrap/"
-                               class="btn btn-outline-white btn-lg">Start free tutorial
-                                <i class="fas fa-graduation-cap ms-2"></i>
-                            </a>
-                        </div>
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleCaptions"
                         data-mdb-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                    <span class="visually-hidden">Trước</span>
                 </button>
                 <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleCaptions"
                         data-mdb-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                    <span class="visually-hidden">Sau</span>
                 </button>
             </div>
             <Products/>
